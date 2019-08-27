d472fed4559776a41d423de56d149184a914aa7a
Joe-PC\Joe <joe790924@gmail.com>
2019-08-28 00:33:54 +0800

    first build

---
+++ precache-manifest.833cdddbb097bc7961d24fb8f9838cce.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "f5bc760c2339f5d3f9a4",
+    "url": "/js/chunk-vendors.b1210503.js"
+  },
+  {
+    "revision": "217790a9fc0ec30b4d35",
+    "url": "/js/app.a4be24db.js"
+  },
+  {
+    "revision": "6e0d45c4f5420aff85fd1d045c918f06",
+    "url": "/index.html"
+  },
+  {
+    "revision": "217790a9fc0ec30b4d35",
+    "url": "/css/app.4069042d.css"
+  }
+];
